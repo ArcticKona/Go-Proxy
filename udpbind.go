705d8ad83243e5a023b89931ce06ceb5910eabf2
Arctic Kona <kona@arcticjieer.fam.cx>
2020-07-13 14:19:45 -0400

    init

--- udpbind.go
+++
@@ -1,9 +0,0 @@
-// Proxy over HTTP. 2020 Arctic Kona. No rights reserved.
-package main
-import "net/http"
-
-// Not implemented
-func proxy_udpbind( response http.ResponseWriter , request * http.Request ) {
-	http.Error( response , "" , 501 )
-	return
-}
