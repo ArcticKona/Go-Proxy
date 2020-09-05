2d68893d93721bdb9494b8a843e5918fd9a5fac9
Arctic Kona <arcticjieer@gmail.com>
2020-09-05 01:54:13 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm client.go
 mv _server.go server.go
-exec go run server.go --server=:$PORT ssh://localhost:22 pptp://localhost:1723 ssh-mobile://localhost:8022
+exec go run server.go --server=:$PORT ssh://localhost:22 pptp://localhost:1723 ssh-mobile://localhost:8022 ssh-kona://localhost:12003
 exit $?
